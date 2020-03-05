0d7654d57cf5bd0594767e9b9cee3532790b35fd
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-03-05 01:22:02 -0500

    envio de arreglo de bytes

--- src/app/components/formcontrol/formcontrol.component.ts
+++ src/app/components/formcontrol/formcontrol.component.ts
@@ -12,6 +12,7 @@ export class FormcontrolComponent implements OnInit {
   valor: string;
 
   formUsuario: FormGroup;
+  srcResult: number[];
 
   /*
    * Para hacerlo mas mantenible podemo hacer uso del FormBuilder
@@ -32,22 +33,22 @@ export class FormcontrolComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.formUsuario.valid){
+    if (this.formUsuario.valid) {
       console.log('recibimos formulario correctamente');
-    }else{
+    } else {
       alert('Formulario invalido');
       return;
     }
   }
 
-  validateFirst(control: AbstractControl){
+  validateFirst(control: AbstractControl) {
     const nombre = control.value;
     let error = null;
-    if (!nombre.includes('$')){
+    if (!nombre.includes('$')) {
       error = {...error, dollar: 'debe ingresar un simbolo dolar'};
     }
-    if (!parseFloat(nombre[0])){
-      error = {...error, number: 'debe comenzar con un numero'}
+    if (!parseFloat(nombre[0])) {
+      error = {...error, number: 'debe comenzar con un numero'};
     }
     return error;
   }
@@ -55,9 +56,57 @@ export class FormcontrolComponent implements OnInit {
   public getError(controlName: string): string {
     let error;
     const control = this.formUsuario.get(controlName);
-    if ((control.touched && control.errors != null)){
+    if ((control.touched && control.errors != null)) {
       error = JSON.stringify(control.errors);
     }
     return error;
   }
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+
+    let result: any[] = [];
+
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+        //result.push(e.target.result);
+        console.log(this.srcResult);
+        const typedArray = new Uint8Array(e.target.result);
+        //const array = [typedArray];
+        const array= Array.from(typedArray);
+        console.log(JSON.stringify(array));
+
+        result.push(array);
+      };
+
+      reader.readAsArrayBuffer(inputNode.files[0]);
+
+      console.log(result);
+
+      let miObject: RequestObject = {numeroDoc: '', plantilla: [12]};
+
+      miObject.numeroDoc = '42';
+      miObject.plantilla = result;
+
+      console.log(miObject);
+
+    }
+  }
+
+  showInterface() {
+    let miObject: RequestObject = {numeroDoc: '', plantilla: [12]};
+
+    miObject.numeroDoc = '42712422';
+    miObject.plantilla = [12, 13];
+
+    console.log(miObject);
+  }
+}
+
+export interface RequestObject {
+  numeroDoc: string;
+  plantilla: number[];
 }
